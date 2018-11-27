56f6893fd287e7e5d772cbe004b0a756d8b71148
alexminer88 <alex.miner@gmail.com>
2018-11-26 20:58:54 -0800

    Query YouTube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,14 @@
+import YOUTUBE_API_KEY from '../config/youtube.js';
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${options.query}&maxResults=${options.max}&key=${options.key}&type=video`,
+    type: 'GET',
+    // success: callback
+    success: function (data) {
+      callback(data.items);
+    }
+  });
 };
 
 export default searchYouTube;
